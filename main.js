972e3014edb39e292f40409bb5e277ef76776bd2
vikharry <vikasg001@gamil.com>
2019-12-05 21:39:45 +0530

    --Renew Files Stuacture

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
+const { check, validationResult } = require('express-validator');
 const port = 3000;
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/public/views');
 
+
+// create application/json parser
+var jsonParser = bodyParser.json();
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.get('/', (req, res) => {
     res.render('pages/index', { title: 'Main Website', message: 'Welcome to templating using EJS' });
@@ -33,6 +40,26 @@ app.get('/service', function (req, res) {
 
 app.get('/login', function (req, res) {
     res.render('pages/login');
+});
+
+// POST /login gets urlencoded bodies
+app.post('/login', urlencodedParser, [
+    check('useremail', 'Your email is not valid').not().isEmpty().isEmail(),
+    check('password', 'Your password must be at least 5 characters').not().isEmpty().isLength({ min: 5 })
+], function (req, res) {
+    const errors = validationResult(req);
+    console.log(req.body, errors.mapped());
+    if (errors.isEmpty()) {
+        res.render('pages/login', { username: req.body.useremail, password: req.body.password });
+    } else {
+        res.render('pages/login', { errors: errors.mapped() });
+    }
+
+});
+
+// POST /api/users gets JSON bodies
+app.post('/api/users', jsonParser, function (req, res) {
+    // create user in req.body
 });
 
 
